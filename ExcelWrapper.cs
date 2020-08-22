992dc1979da1739f9c625c01097e1e04df15f266
Thomas Winkel <ThomasWi@dspace.de>
2020-08-22 15:01:43 +0200

    Indentation to spaces

--- ExcelWrapper.cs
+++ ExcelWrapper.cs
@@ -13,17 +13,17 @@ using System.Reflection;
 
 public class ExcelDemo
 {
-	[Start]
-	public void StartExcel()
-	{
-		// Create new Excel instance and add a workbook.
+    [Start]
+    public void StartExcel()
+    {
+        // Create new Excel instance and add a workbook.
         // Using the Excel Wrapper for EPLAN Scripts this looks like COM Interop:
-		Excel.Application xlsApp = new Excel.Application();
-		xlsApp.Visible = true;
-		xlsApp.Workbooks.Add();
-		
-		// Your code here...
-	}
+        Excel.Application xlsApp = new Excel.Application();
+        xlsApp.Visible = true;
+        xlsApp.Workbooks.Add();
+        
+        // Your code here...
+    }
 }
 
 
@@ -33,51 +33,51 @@ public class ExcelDemo
 // The whole code for an Eplan script must be in one file.
 namespace Excel
 {
-	public class Application
-	{
-		private object _application;
-		
-		public Application()
-		{
-			Type type = Type.GetTypeFromProgID("Excel.Application");
-			_application = Activator.CreateInstance(type);
-		}
-		
-		public bool Visible
-		{
-			get
-			{
-				return (bool)_application.GetType().InvokeMember("Visible", BindingFlags.GetProperty , null, _application, null);
-			}
-			set
-			{
-				_application.GetType().InvokeMember("Visible", BindingFlags.SetProperty, Type.DefaultBinder, _application, new object[] {value});
-			}
-		}
-		
-		public Excel.Workbooks Workbooks
-		{
-			get
-			{
-				object _workbooks = _application.GetType().InvokeMember("Workbooks", BindingFlags.GetProperty , null, _application, null);
-				Excel.Workbooks workbooks = new Excel.Workbooks(_workbooks);
-				return workbooks;
-			}
-		}
-	}
-	
-	public class Workbooks
-	{
-		private object _workbooks;
-		
-		public Workbooks(object workbooks)
-		{
-			_workbooks = workbooks;
-		}
-		
-		public void Add()
-		{
-			_workbooks.GetType().InvokeMember("Add", BindingFlags.InvokeMethod, null, _workbooks, null);
-		}
-	}
+    public class Application
+    {
+        private object _application;
+        
+        public Application()
+        {
+            Type type = Type.GetTypeFromProgID("Excel.Application");
+            _application = Activator.CreateInstance(type);
+        }
+        
+        public bool Visible
+        {
+            get
+            {
+                return (bool)_application.GetType().InvokeMember("Visible", BindingFlags.GetProperty , null, _application, null);
+            }
+            set
+            {
+                _application.GetType().InvokeMember("Visible", BindingFlags.SetProperty, Type.DefaultBinder, _application, new object[] {value});
+            }
+        }
+        
+        public Excel.Workbooks Workbooks
+        {
+            get
+            {
+                object _workbooks = _application.GetType().InvokeMember("Workbooks", BindingFlags.GetProperty , null, _application, null);
+                Excel.Workbooks workbooks = new Excel.Workbooks(_workbooks);
+                return workbooks;
+            }
+        }
+    }
+    
+    public class Workbooks
+    {
+        private object _workbooks;
+        
+        public Workbooks(object workbooks)
+        {
+            _workbooks = workbooks;
+        }
+        
+        public void Add()
+        {
+            _workbooks.GetType().InvokeMember("Add", BindingFlags.InvokeMethod, null, _workbooks, null);
+        }
+    }
 }
